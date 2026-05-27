810ebc488c1aee1dd949b7baa942ccde2a8c3c20
Paw <zaynt16@gmail.com>
2026-05-27 15:54:23 +0700

    Up

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
+import ctypes
+ctypes.windll.user32.SetProcessDPIAware()
+
 import mss
 import numpy as np
 import cv2
 from dotenv import load_dotenv
 import pytesseract
+import pyautogui
 from config import TESSERACT_PATH
 
 load_dotenv()
 pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
 
-with mss.mss() as sct:
+region = {'top': 980, 'left': -1269, 'width': 702, 'height': 109}
 
-    monitor = sct.monitors[1]
-    screenshot = sct.grab(monitor)
+with mss.mss() as sct:
+    print(sct.monitors)
+
+    monitor = sct.monitors[0]
+    screenshot = sct.grab(region)
 
     img = np.array(screenshot)
-    img = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+    gray  = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
 
-    text = pytesseract.image_to_string(img, lang="jpn")
+    cv2.imshow("capture", gray)
+    cv2.waitKey(0)
 
-    print(text)
+
+    print(pyautogui.position())
+
+    # text = pytesseract.image_to_string(gray , lang="jpn")
+    #
+    # print(text)
---
+++ region_selector.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+import mss
+
+start_point = None
+end_point = None
+drawing = False
+
+def mouse_callback(event, x, y, flags, param):
+    global start_point, end_point, drawing
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        start_point = (x, y)
+
+    elif event == cv2.EVENT_MOUSEMOVE and drawing:
+        end_point = (x, y)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        end_point = (x, y)
+
+
+with mss.mss() as sct:
+    print(sct.monitors)
+    monitor = sct.monitors[0]
+    screenshot = np.array(sct.grab(monitor))
+
+img = cv2.cvtColor(screenshot, cv2.COLOR_BGRA2BGR)
+clone = img.copy()
+
+cv2.namedWindow("Select Region", cv2.WINDOW_NORMAL)
+cv2.setMouseCallback("Select Region", mouse_callback)
+
+while True:
+    temp = clone.copy()
+
+    if start_point and end_point:
+        cv2.rectangle(temp, start_point, end_point, (0,255,0), 2)
+
+    cv2.imshow("Select Region", temp)
+
+    key = cv2.waitKey(1)
+
+    if key == 13:  # ENTER untuk confirm
+        break
+
+cv2.destroyAllWindows()
+
+x1, y1 = start_point
+x2, y2 = end_point
+
+region = {
+    "top": min(y1, y2) + monitor["top"],
+    "left": min(x1, x2) + monitor["left"],
+    "width": abs(x2 - x1),
+    "height": abs(y2 - y1),
+}
+
+print("Selected region:")
+print(region)
